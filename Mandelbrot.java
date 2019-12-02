7d02cd778af587026f31e887339cfa9781060fe9
Harry Pinkerton <50304543+hp73@users.noreply.github.com>
2019-12-02 11:26:17 -0500

    added Julia and Mandelbrot combo box

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -110,91 +110,18 @@ public class Mandelbrot extends JFrame implements ActionListener {
         positionConst.gridy = 2;
         add(gradientButton, positionConst);
         
-    
+         // Create Julia/Mandelbrot Combo Box
+        String[] setStrings = {"Mandelbrot Set", "Julia Set"};
         
-
-
-
+        JComboBox setList = new JComboBox(setStrings);
+        setList.setSelectedIndex(0);
         
-        
-        
-      /*  // Create "Triangle" Button
-        increaseButton = new JButton("Increase Limit");
-        increaseButton.addActionListener(this);
-        increaseButton.getText();
-        
-        positionConst.gridx = 10;
-        positionConst.gridy = 10;
-        positionConst.insets = new Insets(0, 0, 0, 0);
-        add(increaseButton, positionConst);
-        
-        */
-        
-       /* // Create "Triangle" Button
-        decreaseButton = new JButton("Decrease Limit");
-        decreaseButton.addActionListener(this);
-        decreaseButton.getText();
-      
-        positionConst.gridx = 0;
-        positionConst.gridy = 2;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(decreaseButton, positionConst);
-    
-        
-        // Create "Triangle" Button
-        resetButton = new JButton("Reset");
-        resetButton.addActionListener(this);
-        resetButton.getText();
-        
-        positionConst.gridx = 0;
-        positionConst.gridy = 3;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(resetButton, positionConst);
-        
-        
-        // Create "Triangle" Button
-        saveImageButton = new JButton("Save Image");
-        saveImageButton.addActionListener(this);
-        saveImageButton.getText();
-        
-        positionConst.gridx = 1;
+        positionConst.gridx = 3;
         positionConst.gridy = 1;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(saveImageButton, positionConst);
+        add(setList, positionConst);
         
-        
-        // Create "Triangle" Button
-        savePosButton = new JButton("Save Position");
-        savePosButton.addActionListener(this);
-        savePosButton.getText();
-
-        positionConst.gridx = 1;
-        positionConst.gridy = 2;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(savePosButton, positionConst);
-        
-        
-        // Create "Triangle" Button
-        loadButton = new JButton("Load Position");
-        loadButton.addActionListener(this);
-        loadButton.getText();
-        
-        positionConst.gridx = 1;
-        positionConst.gridy = 3;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(loadButton, positionConst);
     
-    
-        // Create "Triangle" Button
-        gradientButton = new JButton("Edit Gradient");
-        gradientButton.addActionListener(this);
-        gradientButton.getText();
-        
-        positionConst.gridx = 1;
-        positionConst.gridy = 4;
-        positionConst.insets = new Insets(5, 5, 5, 5);
-        add(gradientButton, positionConst);
-        */
+   
     }
 
     
